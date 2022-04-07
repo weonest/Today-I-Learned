f0bcd70f5303f03f9d795f9a894997dcf2b36f77
weonest <geonhee33@naver.com>
2022-04-07 23:49:04 +0900

    done

--- src/JumpToJava/Main.java
+++ src/JumpToJava/Main.java
@@ -1,23 +1,21 @@
-import java.util.ArrayList;
+package JumpToJava;
 
+import java.util.Arrays;
 
-interface Times {
-    int time(int )
-}
 public class Main {
     public static void main(String[] args) {
-        int[] numbers = {1, 2, 3, 4, 5};
-        ArrayList<Integer> temp = new ArrayList<>();
-        for (int num : numbers) {
-            if (num % 2 == 1) {  // 홀수이면
-                temp.add(num * 2);  // 2를 곱하여 temp에 담는다.
-            }
-        }
 
-        // 정수 리스트를 정수배열로 변환한다.
-        int[] result = new int[temp.size()];
-        for (int i = 0; i < temp.size(); i++) {
-            result[i] = temp.get(i);
-        }
+        String temp = "a:b:c:d";  // 문자열 선언
+        String[] tempArr = temp.split(":");
+
+        
+
+        System.out.println(Arrays.toString(tempArr));
+
+
+
+
     }
 }
+
+
