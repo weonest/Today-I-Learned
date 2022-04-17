8a5e6f7108e29abe5a10433de2d02235c2367c91
weonest <geonhee33@naver.com>
2022-04-17 23:46:33 +0900

    comp

---
+++ src/JumpToJava/Main.java
@@ -0,0 +1,35 @@
+package JumpToJava;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+
+    public Main() {
+    }
+
+    public List<String> getStr(String str) {
+        List<String> list = Arrays.asList(str.split(""));
+        return list;
+    }
+
+    boolean isSame(String str) {
+        str.equals()
+    }
+
+
+    public void count(List<Integer> list) {
+        List<String> cnt = new ArrayList<>();
+        for (int i = 0; i < list.size(); i++) {
+
+        }
+
+    }
+
+}
+
+    public static void main(String[] args) {
+
+    }
+}
