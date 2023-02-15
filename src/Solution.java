9db47728ee6b6ae1b58a1582d049867293c275f8
weonest <geonhee33@naver.com>
2023-02-15 16:13:40 +0900

    프로그래머스 Level1 모의고사

--- src/Solution.java
+++ src/Solution.java
@@ -1,50 +1,44 @@
-
+import java.sql.ClientInfoStatus;
 import java.util.*;
 
 class Solution {
-    public int[] solution(String today, String[] terms, String[] privacies) {
-        ArrayList<Integer> list = new ArrayList<>();
+    public int[] solution(int[] answers) {
 
-        HashMap<String, Integer> map = new HashMap<>();
-        for (String term : terms) {
-            String[] temp = term.split(" ");
-            int date = Integer.parseInt(temp[1]) * 28;
-            map.put(temp[0], date);
+
+        int[] a = {1, 2, 3, 4, 5};
+        int[] b = {2, 1, 2, 3, 2, 4, 2, 5};
+        int[] c = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
+        int[] score = new int[3];
+
+        for (int i = 0; i < answers.length; i++) {
+            if (answers[i] == a[i % 5]) score[0]++;
+            if (answers[i] == b[i % 8]) score[1]++;
+            if (answers[i] == c[i % 10]) score[2]++;
         }
 
-        String[] todaySplited = today.split("\\.");
-        int todayYear = Integer.parseInt(todaySplited[0]);
-        int todayMonth = Integer.parseInt(todaySplited[1]);
-        int todayDate = Integer.parseInt(todaySplited[2]);
+        int max = Math.max(score[0], Math.max(score[1], score[2]));
 
-        int index = 0;
-        for (String data : privacies) {
-            String[] temp = data.split(" ");
-            String term = temp[1];
-            String[] dataSplited = temp[0].split("\\.");
-            int dataYear = Integer.parseInt(dataSplited[0]);
-            int dataMonth = Integer.parseInt(dataSplited[1]);
-            int dataDate = Integer.parseInt(dataSplited[2]);
-
-            int diff = 0;
-            diff += (todayYear - dataYear) * (28 * 12);
-            diff += (todayMonth - dataMonth) * 28;
-            diff += (todayDate - dataDate);
-
-            System.out.println(diff + " " + map.get(term));
-
-            if (map.get(term) <= diff) {
-                list.add(index + 1);
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < score.length; i++) {
+            if (max == score[i]) {
+                list.add(i + 1);
             }
-
-            index++;
         }
-
         int[] answer = new int[list.size()];
         for (int i = 0; i < list.size(); i++) {
             answer[i] = list.get(i);
         }
-
         return answer;
+    }
+
+
+
+
+    public static void main(String[] args) {
+
+        int[] answer = {1,3,2,4,2,3,1,4,2};
+//        int[] answer = {1, 3, 2, 4, 2};
+        Solution sol = new Solution();
+        System.out.println(sol.solution(answer));
     }
 }
