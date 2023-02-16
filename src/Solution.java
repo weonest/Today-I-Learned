cb19607227bd0ed1452d8a4f909d878f85c1aac5
weonest <geonhee33@naver.com>
2023-02-16 23:52:19 +0900

    프로그래머스 Level1 카드 뭉치

--- src/Solution.java
+++ src/Solution.java
@@ -1,47 +1,71 @@
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 class Solution {
-    public int[] solution(String[] id_list, String[] report, int k) {
-        int[] answer = new int[id_list.length];
+    public String solution(String[] cards1, String[] cards2, String[] goal) {
+        String answer = "";
 
-        Map<String, HashSet<String>> reporterMap = new HashMap<>();
-        Map<String, Integer> reportedMap = new HashMap<>();
-        Set<String> reportSet = new HashSet<>(Arrays.asList(report));
-
-        for (String s : reportSet) {
-            String reporter = s.split(" ")[0];
-            String reported = s.split(" ")[1];
-            reporterMap.putIfAbsent(reporter, new HashSet<String>(){{ // 아마도 익명함수
-                add(reported);
-            }});
-            reporterMap.get(reporter).add(reported);
-            reportedMap.put(reported, reportedMap.getOrDefault(reported, 0) + 1);
+        if (checkMethod(cards1, cards2, goal) == true && checkMethod(cards2, cards1, goal) == true) {
+            answer = "Yes";
+        } else {
+            answer = "No";
         }
-
-
-        for (String reported : reportedMap.keySet()) {
-            int reportedCnt = reportedMap.get(reported);
-            if (reportedCnt >= k) {
-                for (int i = 0; i < id_list.length; i++) {
-                    if (reporterMap.containsKey(id_list[i]) && reporterMap.get(id_list[i]).contains(reported)) {
-                        answer[i]++;
-                    }
-                }
-            }
-        }
-
-
         return answer;
     }
 
+    public boolean checkMethod(String[] arr, String[] arr2, String[] target) {
+        boolean flag = true;
+        List<String> tmp = new ArrayList<>(Arrays.asList(target));
+        System.out.println("tmp = " + tmp);
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < tmp.size(); j++) {
+                if (arr[i].equals(tmp.get(j))) {
+                    tmp.remove(j);
+                }
+            }
+        }
+        for (int i = 0; i < tmp.size(); i++) {
+            if (tmp.get(i).equals(arr2[i])) {
+                continue;
+            } else {
+                System.out.println("다릅니다");
+                flag = false;
+            }
+        }
+        return flag;
+    }
+
+    /**
+     * class Solution {
+     *     public String solution(String[] cards1, String[] cards2, String[] goal) {
+     *         List<String> card1Ary = new ArrayList<>(Arrays.asList(cards1));
+     *         List<String> card2Ary = new ArrayList<>(Arrays.asList(cards2));
+     *
+     *         String answer = "Yes";
+     *         for(String str:goal) {
+     *             if(!card1Ary.isEmpty() && card1Ary.get(0).equals(str)) {
+     *                 card1Ary.remove(0);
+     *             } else if(!card2Ary.isEmpty() && card2Ary.get(0).equals(str)) {
+     *                 card2Ary.remove(0);
+     *             } else {
+     *                 answer = "No";
+     *                 break;
+     *             }
+     *         }
+     *         return answer;
+     *     }
+     * }
+     */
+
     public static void main(String[] args) {
 
-//        String[] id_list = {"muzi", "frodo", "apeach", "neo"};
-//        String[] report = {"muzi frodo", "apeach frodo", "frodo neo", "muzi neo", "apeach muzi"};
-        String[] id_list = {"con", "ryan"};
-        String[] report = {"ryan con", "ryan con", "ryan con", "ryan con"};
-        int k = 2;
+        String[] cards1 = {"i", "water", "drink"};
+        String[] cards2 = {"want", "to"};
+        String[] goal = {"i", "want", "to", "drink", "water"};
+
         Solution sol = new Solution();
-        sol.solution(id_list, report, k);
+        System.out.println(sol.solution(cards1, cards2, goal));
     }
 }
