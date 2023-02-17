8a2549e13e58de5e82e42f86f3e87862b0b1297e
weonest <geonhee33@naver.com>
2023-02-17 23:22:21 +0900

    프로그래머스 Level1 명예의 전당

--- src/Solution.java
+++ src/Solution.java
@@ -1,71 +1,25 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 class Solution {
-    public String solution(String[] cards1, String[] cards2, String[] goal) {
-        String answer = "";
+    public int[] solution(int k, int[] score) {
+        int[] answer = new int[score.length];
 
-        if (checkMethod(cards1, cards2, goal) == true && checkMethod(cards2, cards1, goal) == true) {
-            answer = "Yes";
-        } else {
-            answer = "No";
+        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>();
+
+        int temp = 0;
+
+        for(int i = 0; i < score.length; i++) {
+
+            priorityQueue.add(score[i]);
+            if (priorityQueue.size() > k) {
+                priorityQueue.poll();
+            }
+
+            answer[i] = priorityQueue.peek();
         }
+
+
+
         return answer;
-    }
-
-    public boolean checkMethod(String[] arr, String[] arr2, String[] target) {
-        boolean flag = true;
-        List<String> tmp = new ArrayList<>(Arrays.asList(target));
-        System.out.println("tmp = " + tmp);
-
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = 0; j < tmp.size(); j++) {
-                if (arr[i].equals(tmp.get(j))) {
-                    tmp.remove(j);
-                }
-            }
-        }
-        for (int i = 0; i < tmp.size(); i++) {
-            if (tmp.get(i).equals(arr2[i])) {
-                continue;
-            } else {
-                System.out.println("다릅니다");
-                flag = false;
-            }
-        }
-        return flag;
-    }
-
-    /**
-     * class Solution {
-     *     public String solution(String[] cards1, String[] cards2, String[] goal) {
-     *         List<String> card1Ary = new ArrayList<>(Arrays.asList(cards1));
-     *         List<String> card2Ary = new ArrayList<>(Arrays.asList(cards2));
-     *
-     *         String answer = "Yes";
-     *         for(String str:goal) {
-     *             if(!card1Ary.isEmpty() && card1Ary.get(0).equals(str)) {
-     *                 card1Ary.remove(0);
-     *             } else if(!card2Ary.isEmpty() && card2Ary.get(0).equals(str)) {
-     *                 card2Ary.remove(0);
-     *             } else {
-     *                 answer = "No";
-     *                 break;
-     *             }
-     *         }
-     *         return answer;
-     *     }
-     * }
-     */
-
-    public static void main(String[] args) {
-
-        String[] cards1 = {"i", "water", "drink"};
-        String[] cards2 = {"want", "to"};
-        String[] goal = {"i", "want", "to", "drink", "water"};
-
-        Solution sol = new Solution();
-        System.out.println(sol.solution(cards1, cards2, goal));
     }
 }
