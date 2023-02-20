7810c4f77b4e0f3b4dbc6036dca95742b56510cf
weonest <geonhee33@naver.com>
2023-02-20 23:38:57 +0900

    프로그래머스 Level1 성격 유형 검사

--- src/Solution.java
+++ src/Solution.java
@@ -1,83 +1,84 @@
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
-import java.util.Stack;
-import java.util.stream.Collectors;
+import java.util.HashMap;
+import java.util.Map;
 
 class Solution {
+    Map<Integer, Integer> answerMap = new HashMap<>();
+    Map<String, Integer> pes = new HashMap<>();
+    String[] arr = {"RT", "CF", "JM", "AN"};
 
-    public int solution(int[] ingredient) {
-        int answer = 0;
+    public String solution(String[] survey, int[] choices) {
+        String answer = "";
 
-        Stack<Integer> stack = new Stack<>();
+        answerMap.put(1, 3);
+        answerMap.put(2, 2);
+        answerMap.put(3, 1);
+        answerMap.put(4, 0);
+        answerMap.put(5, 1);
+        answerMap.put(6, 2);
+        answerMap.put(7, 3);
 
-        for (int value : ingredient) {
-            stack.push(value);
+        pes.put("R", 0);
+        pes.put("T", 0);
+        pes.put("C", 0);
+        pes.put("F", 0);
+        pes.put("J", 0);
+        pes.put("M", 0);
+        pes.put("A", 0);
+        pes.put("N", 0);
 
-            if (stack.size() >= 4) {
+        int idx = 0;
 
-                if (stack.get(stack.size() - 4) == 1
-                        && stack.get(stack.size() - 3) == 2
-                        && stack.get(stack.size() - 2) == 3
-                        && stack.get(stack.size() - 1) == 1) {
-
-                    System.out.println("stack = " + stack);
-                    answer++;
-                    stack.pop();
-                    stack.pop();
-                    stack.pop();
-                    stack.pop();
-                }
-
-            }
-
+        for (String s : survey) {
+            String[] tmp = s.split("");
+            System.out.println(Arrays.toString(tmp));
+            storeAnswer(tmp, choices[idx]);
+            idx++;
         }
+
+        for (String s : arr) {
+            String[] tmp = s.split("");
+            answer += compareMethod(tmp);
+        }
+
+        System.out.println("pes = " + pes);
+        System.out.println("answer = " + answer);
         return answer;
     }
 
+    public void storeAnswer(String[] survey, int choice) {
+        int tmpAnswer = answerMap.get(choice);
+        String target = "";
+        if (choice > 4) {
+            target = survey[1];
+        } else if (choice < 4) {
+            target = survey[0];
+        } else{
+            return;
+        }
+        pes.replace(target, pes.get(target) + tmpAnswer);
+
+
+    }
+
+    public String compareMethod(String[] tmp) {
+        int a = pes.get(tmp[0]);
+        int b = pes.get(tmp[1]);
+        String tmpAnswer = "";
+        if (a > b || a == b) {
+            tmpAnswer = tmp[0];
+        } else if (a < b) {
+            tmpAnswer= tmp[1];
+        }
+        return tmpAnswer;
+    }
 
     public static void main(String[] args) {
-
-        int[] ingredient = {2, 1, 1, 2, 3, 1, 2, 3, 1};
+        String[] survey = {"AN"};
+        int[] choices = {4};
+//        String[] survey = {"AN", "CF", "MJ", "RT", "NA"};
+//        int[] choices = {5, 3, 2, 7, 5};
         Solution sol = new Solution();
-        sol.solution(ingredient);
+        sol.solution(survey, choices);
     }
 }
-/**
- * class Solution {
- * static int answer = 0;
- * <p>
- * public int solution(int[] ingredient) {
- * <p>
- * String ingre = "";
- * <p>
- * for (Integer value : ingredient) {
- * ingre += value;
- * }
- * <p>
- * makeBurger(ingre);
- * <p>
- * return answer;
- * }
- * <p>
- * public String makeBurger(String ingre) {
- * <p>
- * String remainIngredient = "";
- * <p>
- * if (ingre.length() < 4) {
- * return "end";
- * }
- * <p>
- * int target = ingre.indexOf("1231");
- * <p>
- * if (!ingre.contains("1231")) {
- * return "end";
- * } else if (ingre.contains("1231")) {
- * answer++;
- * remainIngredient += ingre.substring(0, target);
- * remainIngredient += ingre.substring(target + 4, ingre.length());
- * }
- * makeBurger(remainIngredient);
- * return remainIngredient;
- * }
- */
