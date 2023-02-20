cbfde631ee5a575f193e47ae9a6b123029c3fd19
weonest <geonhee33@naver.com>
2023-02-20 17:12:25 +0900

    프로그래머스 Level1 햄버거 만들기

--- src/Solution.java
+++ src/Solution.java
@@ -1,36 +1,83 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Stack;
+import java.util.stream.Collectors;
 
 class Solution {
-    static int answer = 0;
 
-    public int solution(int number, int limit, int power) {
+    public int solution(int[] ingredient) {
+        int answer = 0;
 
-        for (int i = 1; i <= number; i++) {
-            getNum(i, limit, power);
+        Stack<Integer> stack = new Stack<>();
+
+        for (int value : ingredient) {
+            stack.push(value);
+
+            if (stack.size() >= 4) {
+
+                if (stack.get(stack.size() - 4) == 1
+                        && stack.get(stack.size() - 3) == 2
+                        && stack.get(stack.size() - 2) == 3
+                        && stack.get(stack.size() - 1) == 1) {
+
+                    System.out.println("stack = " + stack);
+                    answer++;
+                    stack.pop();
+                    stack.pop();
+                    stack.pop();
+                    stack.pop();
+                }
+
+            }
+
         }
-        System.out.println("answer = " + answer);
         return answer;
     }
 
-    public void getNum(int number, int limit, int power) {
-        int cnt = 0;
-        for (int i = 1; i * i <= number; i++) {
-            if (i * i == number) {
-                cnt++;
-            } else if (number % i == 0) { // 나누려는 몫이 뒤로 들어가야 함
-                cnt += 2;
-            }
-        }
-        if (cnt > limit) {
-            cnt = power;
-        }
-        answer += cnt;
-
-    }
 
     public static void main(String[] args) {
+
+        int[] ingredient = {2, 1, 1, 2, 3, 1, 2, 3, 1};
         Solution sol = new Solution();
-        sol.solution(10, 3, 2);
+        sol.solution(ingredient);
     }
 }
+/**
+ * class Solution {
+ * static int answer = 0;
+ * <p>
+ * public int solution(int[] ingredient) {
+ * <p>
+ * String ingre = "";
+ * <p>
+ * for (Integer value : ingredient) {
+ * ingre += value;
+ * }
+ * <p>
+ * makeBurger(ingre);
+ * <p>
+ * return answer;
+ * }
+ * <p>
+ * public String makeBurger(String ingre) {
+ * <p>
+ * String remainIngredient = "";
+ * <p>
+ * if (ingre.length() < 4) {
+ * return "end";
+ * }
+ * <p>
+ * int target = ingre.indexOf("1231");
+ * <p>
+ * if (!ingre.contains("1231")) {
+ * return "end";
+ * } else if (ingre.contains("1231")) {
+ * answer++;
+ * remainIngredient += ingre.substring(0, target);
+ * remainIngredient += ingre.substring(target + 4, ingre.length());
+ * }
+ * makeBurger(remainIngredient);
+ * return remainIngredient;
+ * }
+ */
