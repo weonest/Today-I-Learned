9cb21ff1b7f9c477ff928b457519cae1c90dd688
weonest <geonhee33@naver.com>
2023-02-22 17:53:12 +0900

    프로그래머스 Level1 신규 아이디 (정규식 풀이)

--- src/Solution.java
+++ src/Solution.java
@@ -1,84 +1,39 @@
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
+import java.io.BufferedReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
 
 class Solution {
-    Map<Integer, Integer> answerMap = new HashMap<>();
-    Map<String, Integer> pes = new HashMap<>();
-    String[] arr = {"RT", "CF", "JM", "AN"};
 
-    public String solution(String[] survey, int[] choices) {
-        String answer = "";
+    public String solution(String new_id) {
 
-        answerMap.put(1, 3);
-        answerMap.put(2, 2);
-        answerMap.put(3, 1);
-        answerMap.put(4, 0);
-        answerMap.put(5, 1);
-        answerMap.put(6, 2);
-        answerMap.put(7, 3);
+        // 1단계 new_id의 모든 대문자를 대응되는 소문자로 치환합니다.
+        new_id = new_id.toLowerCase();
 
-        pes.put("R", 0);
-        pes.put("T", 0);
-        pes.put("C", 0);
-        pes.put("F", 0);
-        pes.put("J", 0);
-        pes.put("M", 0);
-        pes.put("A", 0);
-        pes.put("N", 0);
+        // 2단계 new_id에서 알파벳 소문자, 숫자, 빼기(-), 밑줄(_), 마침표(.)를 제외한 모든 문자 제거
+        new_id = new_id.replaceAll("[^a-z0-9-_.]","");
 
-        int idx = 0;
+        // 3단계 new_id에서 마침표(.)가 2번 이상 연속된 부분을 하나의 마침표(.)로 치환합니다.
+        new_id = new_id.replaceAll("\\.{2,}",".");
 
-        for (String s : survey) {
-            String[] tmp = s.split("");
-            System.out.println(Arrays.toString(tmp));
-            storeAnswer(tmp, choices[idx]);
-            idx++;
+        // 4단계 new_id에서 마침표(.)가 처음이나 끝에 위치한다면 제거합니다.
+        new_id = new_id.replaceAll("^(\\.)|(\\.)$","");
+
+        // 5단계 new_id가 빈 문자열이라면, new_id에 "a"를 대입합니다.
+        if(new_id.isEmpty()) new_id = "a";
+
+        // 6단계 new_id의 길이가 16자 이상이면, 첫 15개의 문자를 제외한 나머지 문자들을 모두 제거
+        //      만약 제거 후 마침표(.)가 new_id의 끝에 위치한다면 끝에 위치한 마침표(.) 문자를 제거
+        if(new_id.length() >= 16) {
+            new_id = new_id.substring(0,15).replaceAll("(\\.)$", "");
         }
 
-        for (String s : arr) {
-            String[] tmp = s.split("");
-            answer += compareMethod(tmp);
+        // 7단계 new_id의 길이가 2자 이하라면,
+        //       new_id의 마지막 문자를 new_id의 길이가 3이 될 때까지 반복해서 끝에 붙입니다.
+        while(new_id.length() < 3) {
+            new_id += new_id.charAt(new_id.length()-1);
         }
 
-        System.out.println("pes = " + pes);
-        System.out.println("answer = " + answer);
-        return answer;
-    }
-
-    public void storeAnswer(String[] survey, int choice) {
-        int tmpAnswer = answerMap.get(choice);
-        String target = "";
-        if (choice > 4) {
-            target = survey[1];
-        } else if (choice < 4) {
-            target = survey[0];
-        } else{
-            return;
-        }
-        pes.replace(target, pes.get(target) + tmpAnswer);
-
-
-    }
-
-    public String compareMethod(String[] tmp) {
-        int a = pes.get(tmp[0]);
-        int b = pes.get(tmp[1]);
-        String tmpAnswer = "";
-        if (a > b || a == b) {
-            tmpAnswer = tmp[0];
-        } else if (a < b) {
-            tmpAnswer= tmp[1];
-        }
-        return tmpAnswer;
-    }
-
-    public static void main(String[] args) {
-        String[] survey = {"AN"};
-        int[] choices = {4};
-//        String[] survey = {"AN", "CF", "MJ", "RT", "NA"};
-//        int[] choices = {5, 3, 2, 7, 5};
-        Solution sol = new Solution();
-        sol.solution(survey, choices);
+        return new_id;
     }
 }
