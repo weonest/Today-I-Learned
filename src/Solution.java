6d92ab82f72f048a0762dbacec63ea280bb54a5b
weonest <geonhee33@naver.com>
2023-03-28 23:27:16 +0900

    프로그래머스 Level2 문자열 압축

--- src/Solution.java
+++ src/Solution.java
@@ -1,60 +1,32 @@
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.Queue;
-
 class Solution {
-    public int solution(int[] queue1, int[] queue2) {
-        int answer = 0;
+    public int solution(String s) {
+        // " abcabcbcbc  "
+        int answer = s.length();
+        int compression = 1;
+        for (int i = 1; i <= s.length() / 2; i++) { // 절반으로 나누는 것이 최대이므로
+            // i <= s.length 해줘야 함
+            StringBuilder sb = new StringBuilder();
+            String target = s.substring(0, i);
 
+            for (int j = i; j <= s.length(); j += i) {
+                int endIdx = Math.min(j + i, s.length());
 
-        Queue<Integer> que1 = new LinkedList<>();
-        Queue<Integer> que2 = new LinkedList<>();
+                String cur = s.substring(j, endIdx); // 0, 1, 2, 3 에서 substring(1,2)면 1만 남음
 
-        long target = 0;
-        long sum1 = 0, sum2 = 0;
-
-
-
-
-        for (Integer i : queue1) {
-            sum1 += i;
-            que1.add(i);
-        }
-
-        for (Integer i : queue2) {
-            sum2 += i;
-            que2.add(i);
-        }
-        target = sum1 + sum2;
-        if (target % 2 != 0) {
-            return -1;
-        }
-
-        target /= 2;
-
-        int cnt = 0;
-
-        while (sum1 != target) {
-            if (sum1 > target) {
-                int tmp = que1.poll(); // 반환 & 삭제
-                sum1 -= tmp;
-                sum2 += tmp;
-                que2.add(tmp);
-                cnt++;
+                if (cur.equals(target)) {
+                    compression++;
+                } else {
+                    if (compression >= 2) {
+                        sb.append(compression);
+                    }
+                    sb.append(target);
+                    target = cur;
+                    compression = 1;
+                }
             }
-            if (sum2 > target) {
-                int tmp = que2.poll();
-                sum2 -= tmp;
-                sum1 += tmp;
-                que1.add(tmp);
-                cnt++;
-            }
-            if (cnt > queue1.length * 3) { // 한계치 = 모든 원소를 교체했는데도 해를 찾지 못한 경우
-                return -1;
-            }
+            sb.append(target);
+            answer = Math.min(answer, sb.length());
         }
-        answer = cnt;
-
         return answer;
     }
 }
