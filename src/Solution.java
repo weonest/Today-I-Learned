3846ffe89ad0fad792b3d65b06da29ea25d7b37a
weonest <geonhee33@naver.com>
2023-03-28 23:38:01 +0900

    프로그래머스 Level1 나머지가 1이 되는 수

--- src/Solution.java
+++ src/Solution.java
@@ -1,32 +1,27 @@
 class Solution {
-    public int solution(String s) {
-        // " abcabcbcbc  "
-        int answer = s.length();
-        int compression = 1;
-        for (int i = 1; i <= s.length() / 2; i++) { // 절반으로 나누는 것이 최대이므로
-            // i <= s.length 해줘야 함
-            StringBuilder sb = new StringBuilder();
-            String target = s.substring(0, i);
+    public int solution(int n) {
+        int answer = 0;
 
-            for (int j = i; j <= s.length(); j += i) {
-                int endIdx = Math.min(j + i, s.length());
 
-                String cur = s.substring(j, endIdx); // 0, 1, 2, 3 에서 substring(1,2)면 1만 남음
 
-                if (cur.equals(target)) {
-                    compression++;
-                } else {
-                    if (compression >= 2) {
-                        sb.append(compression);
-                    }
-                    sb.append(target);
-                    target = cur;
-                    compression = 1;
-                }
+        for (int i = n-1; i > 0; i--) {
+            if (n % i == 1) {
+                answer = i;
             }
-            sb.append(target);
-            answer = Math.min(answer, sb.length());
+        }
+
+        return answer;
+    }
+
+    public int solution2(int n) {
+        int answer = 0;
+        for (int i=2; i<n; i++) {
+            if (n % i == 1) {
+                answer = i;
+                break; // break를 통해 최소 값에서 멈추기 때문에
+            }
         }
         return answer;
     }
+
 }
