c0fc7e3271b45417aa472caaf9d6e3fe24ce0572
weonest <geonhee33@naver.com>
2023-03-29 22:53:16 +0900

    프로그래머스 Level1 덧칠하기

--- src/Solution.java
+++ src/Solution.java
@@ -1,37 +1,15 @@
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
-
 class Solution {
-    public int[] solution(String[] wallpaper) {
-        int[] answer = new int[4];
 
+    public int solution(int n, int m, int[] section) {
+        int answer = 0;
+        int max = 0;
 
-        List<Integer> x = new ArrayList<>();
-        List<Integer> y = new ArrayList<>();
-
-        for (int i = 0; i < wallpaper.length; i++) {
-            String[] str = wallpaper[i].split("");
-
-            for (int j = 0; j < str.length; j++) {
-                if (str[j].equals("#")) {
-                    x.add(j);
-                    y.add(i);
-                }
+        for (int i = 0; i < section.length; i++) {
+            if (section[i] > max) {
+                answer++;
+                max = section[i] + m - 1;
             }
         }
-        x.sort(Comparator.naturalOrder());
-        y.sort(Comparator.naturalOrder());
-
-        answer[0] = y.get(0);
-        answer[1] = x.get(0);
-        answer[2] = y.get(y.size() - 1) + 1;
-        answer[3] = x.get(x.size() - 1) + 1;
-
-        System.out.println("x = " + x);
-        System.out.println("y = " + y);
-
-
         return answer;
     }
 }
