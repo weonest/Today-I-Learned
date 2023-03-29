cbd667b7f32919591b048c772800c9561caee596
weonest <geonhee33@naver.com>
2023-03-29 22:05:06 +0900

    프로그래머스 Level1 바탕화면 정리

--- src/Solution.java
+++ src/Solution.java
@@ -1,27 +1,37 @@
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
 class Solution {
-    public int solution(int n) {
-        int answer = 0;
+    public int[] solution(String[] wallpaper) {
+        int[] answer = new int[4];
 
 
+        List<Integer> x = new ArrayList<>();
+        List<Integer> y = new ArrayList<>();
 
-        for (int i = n-1; i > 0; i--) {
-            if (n % i == 1) {
-                answer = i;
+        for (int i = 0; i < wallpaper.length; i++) {
+            String[] str = wallpaper[i].split("");
+
+            for (int j = 0; j < str.length; j++) {
+                if (str[j].equals("#")) {
+                    x.add(j);
+                    y.add(i);
+                }
             }
         }
+        x.sort(Comparator.naturalOrder());
+        y.sort(Comparator.naturalOrder());
+
+        answer[0] = y.get(0);
+        answer[1] = x.get(0);
+        answer[2] = y.get(y.size() - 1) + 1;
+        answer[3] = x.get(x.size() - 1) + 1;
+
+        System.out.println("x = " + x);
+        System.out.println("y = " + y);
+
 
         return answer;
     }
-
-    public int solution2(int n) {
-        int answer = 0;
-        for (int i=2; i<n; i++) {
-            if (n % i == 1) {
-                answer = i;
-                break; // break를 통해 최소 값에서 멈추기 때문에
-            }
-        }
-        return answer;
-    }
-
 }
