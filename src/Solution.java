10d0059b7cc04468019cb647b5f408feab28c5c1
weonest <geonhee33@naver.com>
2023-03-29 23:40:07 +0900

    프로그래머스 Level1 대충 만든 자판

--- src/Solution.java
+++ src/Solution.java
@@ -1,15 +1,44 @@
+import java.util.*;
+
 class Solution {
 
-    public int solution(int n, int m, int[] section) {
-        int answer = 0;
-        int max = 0;
+    public int[] solution(String[] keymap, String[] targets) {
+        int[] answer = new int[targets.length];
 
-        for (int i = 0; i < section.length; i++) {
-            if (section[i] > max) {
-                answer++;
-                max = section[i] + m - 1;
+        Map<Character, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < keymap.length; i++) {
+            String str = keymap[i];
+
+            for (int j = 0; j < str.length(); j++) {
+                char c = str.charAt(j);
+                if (map.containsKey(c)) {
+                    map.put(c, Math.min(j + 1, map.get(c)));
+                } else {
+                    map.put(c, j + 1);
+                }
             }
         }
+
+        System.out.println("map = " + map);
+
+
+        for (int i = 0; i < targets.length; i++) {
+            String str = targets[i];
+            for (int j = 0; j < str.length(); j++) {
+                char c = str.charAt(j);
+
+                if (map.containsKey(c)) {
+                    answer[i] += map.get(c);
+                }else {
+                    answer[i] = -1;
+                    break;
+                }
+
+                System.out.println("map.get(c) = " + map.get(c));
+            }
+        }
+
         return answer;
     }
 }
