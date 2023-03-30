403982867e53b1b630493158881a4dd7c0a92430
weonest <geonhee33@naver.com>
2023-03-30 23:24:02 +0900

    프로그래머스 Level1 과일 장수

--- src/Solution.java
+++ src/Solution.java
@@ -1,29 +1,44 @@
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.LinkedList;
+import java.util.Queue;
 
 class Solution {
-    public int[] solution(String[] name, int[] yearning, String[][] photo) {
-        int[] answer = new int[photo.length];
+    public int solution(int k, int m, int[] score) {
+        int answer = 0;
+        int[] box = new int[m];
+        int boxLimit = score.length / m;
 
+        Queue<Integer> que = new LinkedList<>();
 
-        Map<String, Integer> map = new HashMap<>();
+        Integer[] tmp = Arrays.stream(score).boxed().toArray(Integer[]::new);
+        Arrays.sort(tmp, Comparator.reverseOrder());
 
-        for (int i = 0; i < name.length; i++) {
-            map.put(name[i], yearning[i]);
+        for (Integer i : tmp) {
+            que.add(i);
         }
 
-        for (int i = 0; i < photo.length; i++) {
-
-            for (int j = 0; j < photo[i].length; j++) {
-                if (map.containsKey(photo[i][j])) {
-                    answer[i] += map.get(photo[i][j]);
-                } else {
-                    continue;
-                }
+        for (int i = 0; i < boxLimit; i++) {
+            for (int j = 0; j < box.length; j++) {
+                box[j] = que.poll();
             }
+            int min = Arrays.stream(box).min().orElse(0);
+
+            answer += min * m;
         }
 
 
         return answer;
     }
+
+    /**
+     * 더 좋은 풀이
+     */
+
+    int answer = 0;
+    Arrays.sort(score);
+    for (int i = score.length-m; i >= 0; i-=m) {
+        answer += score[i] * m;
+    }
+
 }
