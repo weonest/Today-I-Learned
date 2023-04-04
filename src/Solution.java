3a587c9c0f438fdef42310996b1a242b102aa6a9
weonest <geonhee33@naver.com>
2023-04-04 22:20:22 +0900

    프로그래머스 Level1 로또의 최고 순위와 최저 순위

--- src/Solution.java
+++ src/Solution.java
@@ -1,22 +1,53 @@
-class Solution {
-    public int solution(int left, int right) {
-        int answer = 0;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
-        for (int i = left; i <= right; i++) {
-            int cnt = 0;
-            for (int j = 1; j <= i; j++) {
-                if (i % j == 0) {
-                    cnt++;
-                }
+class Solution {
+    static int[] answer = new int[2];
+
+    public int[] solution(int[] lottos, int[] win_nums) {
+
+        List<Integer> list = Arrays.stream(lottos).boxed().collect(Collectors.toList());
+
+        int wild = 0;
+        int cnt = 0;
+
+        for (int i = 0; i < win_nums.length; i++) {
+
+            if (list.get(i) == 0) {
+                wild++;
             }
-            System.out.println("cnt = " + cnt);
-            if (cnt % 2 == 0) {
-                answer += i;
-            } else {
-                answer -= i;
+            if (list.contains(win_nums[i])) {
+                cnt++;
             }
+
+            checkWin(0, cnt+wild);
+            checkWin(1, cnt);
+
         }
 
         return answer;
+    }
+
+    public void checkWin(int div, int num) {
+        switch (num) {
+            case 6:
+                answer[div] = 1;
+                break;
+            case 5:
+                answer[div] = 2;
+                break;
+            case 4:
+                answer[div] = 3;
+                break;
+            case 3:
+                answer[div] = 4;
+                break;
+            case 2:
+                answer[div] = 5;
+                break;
+            default:
+                answer[div] = 6;
+        }
     }
 }
