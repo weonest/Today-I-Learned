8dd59163ad76415d755d18300b0fc7d730cce060
weonest <geonhee33@naver.com>
2023-04-04 21:05:47 +0900

    프로그래머스 Level1 없는 숫자 더하기

--- src/Solution.java
+++ src/Solution.java
@@ -1,18 +1,27 @@
 import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 class Solution {
-    public int solution(int[][] sizes) {
-        int answer = 0;
+    public int solution(int[] numbers) {
+        int answer = -1;
 
-        int max = 0, min = 0;
+        int[] arr = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
 
-        for (int[] size : sizes) {
-            max = Math.max(max, Math.max(size[0], size[1]));
-            min = Math.max(min, Math.min(size[0], size[1]));
+        List<Integer> list = Arrays.stream(arr).boxed().collect(Collectors.toList());
+
+        System.out.println("list = " + list);
+
+        for (int i = 0; i < numbers.length; i++) {
+            if (list.contains(numbers[i])) {
+                list.remove(list.indexOf(numbers[i]));
+            }
         }
-
-        answer = max * min;
+        if (list.size() > 0) answer = list.stream().mapToInt(Integer::intValue).sum();
 
         return answer;
     }
+
+    // 초간단 풀이..
+    // return 45 - Array.stream(arr).sum();
 }
