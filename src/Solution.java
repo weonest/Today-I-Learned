c3a3387eb3a7ee79e5169ec1b32c939bc3f0b49e
weonest <geonhee33@naver.com>
2023-04-04 22:59:16 +0900

    프로그래머스 Level2 카펫

--- src/Solution.java
+++ src/Solution.java
@@ -1,53 +1,34 @@
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 class Solution {
-    static int[] answer = new int[2];
+    public int[] solution(int brown, int yellow) {
+        int[] answer = new int[2];
 
-    public int[] solution(int[] lottos, int[] win_nums) {
+        int sum = brown + yellow;
 
-        List<Integer> list = Arrays.stream(lottos).boxed().collect(Collectors.toList());
+        int sqrt = (int) Math.sqrt(sum);
 
-        int wild = 0;
-        int cnt = 0;
+        List<Integer> list = new ArrayList<>();
 
-        for (int i = 0; i < win_nums.length; i++) {
-
-            if (list.get(i) == 0) {
-                wild++;
+        for (int i = 1; i <= sqrt; i++) {
+            if (sum % i == 0) {
+                list.add(i);
+                    list.add(sum / i);
             }
-            if (list.contains(win_nums[i])) {
-                cnt++;
+        }
+        System.out.println("list = " + list);
+
+        for (int i = 0; i < list.size(); i+=2) {
+            int tmp = list.get(i);
+            for (int j = 1; j < list.size(); j+=2) {
+                if (tmp + list.get(j) - 2 == brown / 2) {
+                    answer[0] = list.get(j);
+                    answer[1] = list.get(i);
+                }
             }
-
-            checkWin(0, cnt+wild);
-            checkWin(1, cnt);
-
         }
 
         return answer;
-    }
-
-    public void checkWin(int div, int num) {
-        switch (num) {
-            case 6:
-                answer[div] = 1;
-                break;
-            case 5:
-                answer[div] = 2;
-                break;
-            case 4:
-                answer[div] = 3;
-                break;
-            case 3:
-                answer[div] = 4;
-                break;
-            case 2:
-                answer[div] = 5;
-                break;
-            default:
-                answer[div] = 6;
-        }
     }
 }
