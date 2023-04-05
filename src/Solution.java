534b56e68c1d639d818e0bb95c7deb07373501ef
weonest <geonhee33@naver.com>
2023-04-05 21:44:26 +0900

    프로그래머스 Level2 멀리뛰기

--- src/Solution.java
+++ src/Solution.java
@@ -1,19 +1,40 @@
-import java.util.*;
+class Solution {
+    int answer = 0;
 
-public class Solution {
-    public int solution(int n) {
+//    public long solution(int n) {
+//
+//
+//        dfs(n,0);
+//
+//        answer %= 1234567;
+//        return answer;
+//    }
+//
+//    public void dfs(int n, int tmp) {
+//        if (tmp >= n) {
+//            if (tmp == n) {
+//                answer++;
+//            }
+//            return;
+//        }
+//
+//        dfs(n, tmp + 1);
+//        dfs(n, tmp + 2);
+//    }
 
-        int cnt = 1;
+    /**
+     * 위의 재귀로 푸는 식은 정답을 구할 순 있지만, 테스트 케이스에 따라 시간 초과
+     */
+    public long solution(int n) {
+        long arr[] = new long[2001];
+        arr[1] = 1;
+        arr[2] = 2;
 
-        while (n != 1) {
-            if (n % 2 == 0) {
-                n = n / 2;
-            }else {
-                n -= 1;
-                cnt++;
-            }
+        for (int i = 3; i < 2001; i++) {
+            arr[i] = (arr[i-1]+arr[i-2])%1234567;
         }
-
-        return cnt;
+        return arr[n];
     }
+
+
 }
