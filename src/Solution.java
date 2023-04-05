f0d948baa540e7d23dfbeb7cd00069db289d562f
weonest <geonhee33@naver.com>
2023-04-05 20:46:58 +0900

    프로그래머스 Level2 점프와 순간 이동

--- src/Solution.java
+++ src/Solution.java
@@ -1,34 +1,19 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
-class Solution {
-    public int[] solution(int brown, int yellow) {
-        int[] answer = new int[2];
+public class Solution {
+    public int solution(int n) {
 
-        int sum = brown + yellow;
+        int cnt = 1;
 
-        int sqrt = (int) Math.sqrt(sum);
-
-        List<Integer> list = new ArrayList<>();
-
-        for (int i = 1; i <= sqrt; i++) {
-            if (sum % i == 0) {
-                list.add(i);
-                    list.add(sum / i);
-            }
-        }
-        System.out.println("list = " + list);
-
-        for (int i = 0; i < list.size(); i+=2) {
-            int tmp = list.get(i);
-            for (int j = 1; j < list.size(); j+=2) {
-                if (tmp + list.get(j) - 2 == brown / 2) {
-                    answer[0] = list.get(j);
-                    answer[1] = list.get(i);
-                }
+        while (n != 1) {
+            if (n % 2 == 0) {
+                n = n / 2;
+            }else {
+                n -= 1;
+                cnt++;
             }
         }
 
-        return answer;
+        return cnt;
     }
 }
