7d8da45a77623e2df5226bb44bda812253f56298
weonest <geonhee33@naver.com>
2023-04-08 22:19:25 +0900

    프로그래머스 Level2 N개의 최소공배수

--- src/Solution.java
+++ src/Solution.java
@@ -1,40 +1,50 @@
 class Solution {
-    int answer = 0;
+    public int solution(int[] arr) {
+        int answer = arr[0];
 
-//    public long solution(int n) {
+        for (int i = 0; i < arr.length; i++) {
+            answer = answer * arr[i] / gcd(answer, arr[i]);
+        }
+
+
+        return answer;
+    }
+
+    // 1. 최대 공약수를 구하는 식 만들기
+
+    public int gcd(int x, int y) {
+        int gcd = 0;
+        for (int i = x; i > 0; i--) {
+            if (x % i == 0 && y % i == 0) {
+                gcd = i;
+                break;
+            }
+        }
+        return gcd;
+    }
+}
+
+
+//    public int solution(int[] arr) {
+//        int answer = arr[0];
 //
+//        for (int i = 0; i < arr.length; i++) {
 //
-//        dfs(n,0);
-//
-//        answer %= 1234567;
+//            int gcd = gcd(answer, arr[i]); // 최대공약수
+//            answer = answer * arr[i] / gcd;
+//        }
 //        return answer;
 //    }
 //
-//    public void dfs(int n, int tmp) {
-//        if (tmp >= n) {
-//            if (tmp == n) {
-//                answer++;
-//            }
-//            return;
-//        }
+//    public static int gcd(int a, int b) {
 //
-//        dfs(n, tmp + 1);
-//        dfs(n, tmp + 2);
+//        int x = Math.max(a, b);
+//        int y = Math.min(a, b);
+//
+//        while (x % y != 0) { // 나머지가 0이 될 때까지
+//            int r = x % y;
+//            x = y;
+//            y = r;
+//        }
+//        return y;
 //    }
-
-    /**
-     * 위의 재귀로 푸는 식은 정답을 구할 순 있지만, 테스트 케이스에 따라 시간 초과
-     */
-    public long solution(int n) {
-        long arr[] = new long[2001];
-        arr[1] = 1;
-        arr[2] = 2;
-
-        for (int i = 3; i < 2001; i++) {
-            arr[i] = (arr[i-1]+arr[i-2])%1234567;
-        }
-        return arr[n];
-    }
-
-
-}
