a25adbc6dc44007da03ee28845ec3f9c0269e86d
weonest <geonhee33@naver.com>
2023-04-08 23:51:13 +0900

    프로그래머스 Level2 최댓값과 최솟값

--- src/Solution.java
+++ src/Solution.java
@@ -1,50 +1,19 @@
+import java.util.Arrays;
+import java.util.NoSuchElementException;
+
 class Solution {
-    public int solution(int[] arr) {
-        int answer = arr[0];
+    public String solution(String s) {
+        String answer = "";
 
-        for (int i = 0; i < arr.length; i++) {
-            answer = answer * arr[i] / gcd(answer, arr[i]);
-        }
+        String[] arr = s.split(" ");
+
+        int[] nums = Arrays.stream(arr).mapToInt(Integer::parseInt).toArray();
+
+        int min = Arrays.stream(nums).map(x -> x).min().orElseThrow(NoSuchElementException::new);
+
+        int max = Arrays.stream(nums).map(x -> x).max().orElseThrow(NoSuchElementException::new);
 
 
-        return answer;
-    }
-
-    // 1. 최대 공약수를 구하는 식 만들기
-
-    public int gcd(int x, int y) {
-        int gcd = 0;
-        for (int i = x; i > 0; i--) {
-            if (x % i == 0 && y % i == 0) {
-                gcd = i;
-                break;
-            }
-        }
-        return gcd;
+        return answer += min + " " + max;
     }
 }
-
-
-//    public int solution(int[] arr) {
-//        int answer = arr[0];
-//
-//        for (int i = 0; i < arr.length; i++) {
-//
-//            int gcd = gcd(answer, arr[i]); // 최대공약수
-//            answer = answer * arr[i] / gcd;
-//        }
-//        return answer;
-//    }
-//
-//    public static int gcd(int a, int b) {
-//
-//        int x = Math.max(a, b);
-//        int y = Math.min(a, b);
-//
-//        while (x % y != 0) { // 나머지가 0이 될 때까지
-//            int r = x % y;
-//            x = y;
-//            y = r;
-//        }
-//        return y;
-//    }
