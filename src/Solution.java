7a31f8be83381cb4eff28aa22fead2c82dd0e9f8
weonest <geonhee33@naver.com>
2023-04-10 21:23:20 +0900

    프로그래머스 Level2 피보나치 수

--- src/Solution.java
+++ src/Solution.java
@@ -1,22 +1,19 @@
 class Solution {
+
+    int[] arr;
     public int solution(int n) {
         int answer = 0;
+        arr = new int[n + 1];
 
+        return answer = recursive(n);
+    }
 
-        int target = Integer.bitCount(n);
+    public int recursive(int num) {
+        if (arr[num] > 0) return arr[num];
 
-        int tmp = n;
-
-        while (true) {
-            tmp++;
-            int cur = Integer.bitCount(tmp);
-            if (target == cur) {
-                answer = tmp;
-                break;
-            }
+        if (num < 2) {
+            return num;
         }
-
-
-        return answer;
+        return arr[num] = (recursive(num - 2) + recursive(num-1)) % 1234567;
     }
 }
