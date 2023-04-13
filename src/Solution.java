f5498ddd296f21f320d4a39daa55fa6dde25d581
weonest <geonhee33@naver.com>
2023-04-13 23:45:20 +0900

    프로그래머스 Level1 달리기 경주

--- src/Solution.java
+++ src/Solution.java
@@ -1,46 +1,32 @@
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 class Solution {
-    int n = 0;
-    
-    public String[] solution(int n, int[] arr1, int[] arr2) {
-        String[] answer = new String[n];
-        this.n = n;
+    public String[] solution(String[] players, String[] callings) {
+        String[] answer = new String[players.length];
 
-        for (int i = 0; i < n; i++) {
-            String bin1 = Integer.toBinaryString(arr1[i]);
-            String bin2 = Integer.toBinaryString(arr2[i]);
+        Map<Integer, String> map = new HashMap<>();
 
-            String cod1 = plusLength(bin1);
-            String cod2 = plusLength(bin2);
+        int rank = 1;
+        for (String val : players) {
+            map.put(rank, val);
+            rank++;
+        }
+        System.out.println("map = " + map);
 
-            String row = "";
+        for (int i = 1; i < callings.length; i++) {
 
-            for (int j = 0; j < n; j++) {
-                String cur1 = cod1.charAt(j) + "";
-                String cur2 = cod2.charAt(j) + "";
+            if ()
 
-                if (cur1.equals("1") || cur2.equals("1")) {
-                    row += "#";
-                }else{
-                    row += " ";
-                }
             }
-            answer[i] = row;
 
         }
+
+
 
         return answer;
     }
 
-    public String plusLength(String bin) {
-        if (bin.length() != n) {
-            String zero = "";
-            for (int i = 0; i < n - bin.length(); i++) {
-                zero += "0";
-            }
-            bin = zero + bin;
-        }
-        return bin;
-    }
 }
