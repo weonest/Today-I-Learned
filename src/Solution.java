3261edfd3de732038353a04a5d6e0a8190fbcfce
weonest <geonhee33@naver.com>
2023-04-20 21:43:46 +0900

    프로그래머스 Level1 신규아이디 추천

--- src/Solution.java
+++ src/Solution.java
@@ -1,40 +1,53 @@
-import java.util.*;
-
 class Solution {
-    public int solution(int bridge_length, int weight, int[] truck_weights) {
-        Queue<Integer> queue = new LinkedList<>();
-        int sum = 0;
-        int time = 0;
+    public String solution(String new_id) {
+        String answer = "";
 
-        for(int i = 0; i < truck_weights.length; i++) { // 향상된 for문을 쓰는게 좋을 것
-            int truck = truck_weights[i];
+        String s = new_id.toLowerCase();
+        s = s.replaceAll("[^a-z-_.0-9]", "");
 
-            while(true) {
-                // 큐에 아무것도 없는 경우 = 어떠한 트럭도 다리위에 없음
-                if(queue.isEmpty()) {
-                    queue.add(truck);
-                    sum += truck;
-                    time++; // 다리에 오를 때만 시간 추가
-                    break;
-                } else if(queue.size() == bridge_length) { // 큐에 다리 길이만큼 트럭이 다 찬 경우
-                    sum -= queue.poll();
-                } else  { // 다리 길이만큼 큐가 차지않았음
-                    // weight 값을 넘지 않는 선에서 새로운 트럭을 다리에 올려줌
-                    if(sum + truck <= weight) {
-                        queue.add(truck);
-                        sum += truck;
-                        time++;
-                        break;
-                    } else {
-                        // 넘는다면 0을 넣어 이미 큐에 있는 트럭이 다리를 건너게 만듬
-                        queue.add(0);
-                        time++;
-                    }
+        StringBuilder sb = new StringBuilder();
+
+        int cnt = 0;
+        for (int i = 0; i < s.length(); i++) {
+            char cur = s.charAt(i);
+            if (cur == '.') {
+                cnt++;
+                if (cnt > 1) {
+                    continue;
+                } else {
+                    sb.append(cur);
                 }
+            } else {
+                sb.append(cur);
+                cnt = 0;
             }
         }
 
-        // 마지막 트럭에서 반복문이 끝나는데, 마지막 역시 다리 길이만큼 지나가야하기에 + 다리 길이
-        return time + bridge_length;
+
+        if (sb.charAt(0) == '.') {
+            sb.deleteCharAt(0);
+        }
+
+        if (sb.length() > 0) {
+            if (sb.charAt(sb.length() - 1) == '.') {
+                sb.setLength(sb.length() - 1);
+            }
+        }
+
+        if (sb.length() == 0) {
+            sb.append("a");
+        }
+
+        if (sb.length() > 15) {
+            sb.setLength(15);
+            if (sb.charAt(sb.length() - 1) == '.') {
+                sb.setLength(sb.length() - 1);
+            }
+        }
+
+        if (sb.length() <= 2) {
+            while (sb.length() < 3) sb.append(sb.charAt(sb.length() - 1));
+        }
+        return answer = sb.toString();
     }
 }
