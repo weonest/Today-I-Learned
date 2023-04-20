c5d14217c9c9418d8e8fd83b1605342ff01e1ff3
weonest <geonhee33@naver.com>
2023-04-21 00:45:48 +0900

    백준 10828 스택

--- src/Main.java
+++ src/Main.java
@@ -13,31 +13,101 @@ public class Main {
 
         int N = Integer.parseInt(br.readLine());
 
+        Stack<Integer> stack = new Stack<>();
+
         for (int i = 0; i < N; i++) {
-            Stack<Character> stack = new Stack<>();
-            String bracket = br.readLine();
+            String[] command = br.readLine().split(" ");
 
-            for (int j = 0; j < bracket.length(); j++) {
-                char cur = bracket.charAt(j);
 
-                if (cur == '(') {
-                    stack.push(cur);
-                } else if (!stack.isEmpty() && cur == ')') {
-                    if (stack.peek() == ')'){
-                        continue;
-                    }
-                    stack.pop();
-                } else {
-                    stack.push(cur);
-                }
-            }
-            if (stack.isEmpty()) {
-                System.out.println("YES");
-            } else {
-                System.out.println("NO");
+            switch (command[0]) {
+                case "push":
+                    push(stack, Integer.parseInt(command[1]));
+                    break;
+                case "pop":
+                    System.out.println(pop(stack));
+                    break;
+                case "size":
+                    System.out.println(size(stack));
+                    break;
+                case "empty":
+                    System.out.println(empty(stack));
+                    break;
+                case "top":
+                    System.out.println(top(stack));
+                    break;
             }
 
         }
     }
+
+    public static void push(Stack<Integer> stack, int n) {
+        stack.push(n);
+    }
+
+    public static int pop(Stack<Integer> stack) {
+        int n = -1;
+        if (!stack.isEmpty()) {
+            n = stack.pop();
+        }
+        return n;
+    }
+
+    public static int size(Stack<Integer> stack) {
+        int n = 0;
+        if (!stack.isEmpty()) n = stack.size();
+        return n;
+    }
+
+    public static int empty(Stack<Integer> stack) {
+        int n = 0;
+        if (stack.isEmpty()) n = 1;
+        return n;
+    }
+
+    public static int top(Stack<Integer> stack) {
+        int n = -1;
+        if (!stack.isEmpty()) {
+            n = stack.peek();
+        }
+        return n;
+    }
 }
+//public class Main {
+//	public static void main(String[] args) throws IOException {
+//		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+//
+//		int n = Integer.parseInt(br.readLine());
+//
+//        // Stack 객체 생성.
+//		Stack<Integer> stack = new Stack<>();
+//
+//        // 동일.
+//		for(int i = 0;i<n;i++) {
+//			String cons = br.readLine();
+//
+//			if(cons.contains("push")) { // 동일.
+//				String spt[] = cons.split(" ");
+//				stack.push(Integer.parseInt(spt[1]));
+//			}else if(cons.contains("pop")) { // 동일.
+//				if(stack.empty()) bw.write(-1+"\n"); // 별도의 empty() 체크가 필요하다.
+//				else bw.write(stack.pop()+"\n");
+//			}else if(cons.contains("size")) { // 동일.
+//				bw.write(stack.size()+"\n");
+//			}else if(cons.contains("empty")) { // 동일.
+//				if(stack.empty()) bw.write(1+"\n"); // 별도의 empty() 체크가 필요하다.
+//				else bw.write(0+"\n");
+//			}else if(cons.contains("top")) { // 동일.
+//				if(stack.empty())bw.write(-1+"\n");	// 별도의 empty() 체크가 필요하다.
+//				else bw.write(stack.peek()+"\n");
+//			}
+//		}
+//
+//		bw.flush();
+//		bw.close();
+//		br.close();
+//
+//	}
+//
+//}
  
