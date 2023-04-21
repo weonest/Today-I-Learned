20d9ea44f23bc752dda8755e2795a58c431e0508
weonest <geonhee33@naver.com>
2023-04-21 20:03:25 +0900

    프로그래머스 Lv2 할인 행사

--- src/Solution.java
+++ src/Solution.java
@@ -1,80 +1,36 @@
-import java.util.*;
-import java.util.regex.Pattern;
-import java.util.stream.Collectors;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 class Solution {
-    public int solution(String str1, String str2) {
+    public int solution(String[] want, int[] number, String[] discount) {
         int answer = 0;
 
-        str1 = str1.toLowerCase();
-        str2 = str2.toLowerCase();
+        List<String> wantList = new ArrayList<>();
 
+        for (int i = 0; i < want.length; i++) {
 
-        List<String> list1 = new LinkedList<>();
-        List<String> list2 = new LinkedList<>();
+            for (int j = 0; j < number[i]; j++) {
+                wantList.add(want[i]);
+            }
+        }
+        // 1 2 3 4 5 6 7 8 9 10 11 12 13 14
 
+        for (int i = 0; i < discount.length - 9; i++) {
+            List<String> disList = new ArrayList<>();
+            for (int j = i; j < i + 10; j++) {
+                disList.add(discount[j]);
+            }
 
-        String pt = "^[a-z]";
-
-        for (int i = 0; i < str1.length() - 1; i++) {
-            String cur = str1.charAt(i) + "";
-            String nex = str1.charAt(i + 1) + "";
-
-
-            if (Pattern.matches(pt, cur) && Pattern.matches(pt, nex)) {
-                list1.add(cur + nex);
+            for (String s : wantList) {
+                disList.remove(s);
+            }
+            if (disList.isEmpty()) {
+                answer++;
             }
 
         }
-
-        for (int i = 0; i < str2.length() - 1; i++) {
-            String cur = str2.charAt(i) + "";
-            String nex = str2.charAt(i + 1) + "";
-
-
-            if (Pattern.matches(pt, cur) && Pattern.matches(pt, nex)) {
-                list2.add(cur + nex);
-            }
-
-        }
-
-        if (list1.size() >= list2.size()) {
-            answer = (int) Math.floor(jaccard(list1, list2) * 65536);
-        } else {
-            answer = (int) Math.floor(jaccard(list2, list1) * 65536);
-        }
-
 
         return answer;
-    }
-
-    public double jaccard(List<String> list, List<String> list2) {
-
-        Queue<String> que = new LinkedList<>(list2);
-
-        if (list.isEmpty() && list2.isEmpty()) {
-            return 1;
-        }
-
-        double intersection = 0.0;
-        double union = list.size();
-
-        while (!que.isEmpty()) {
-            if (list.contains(que.peek())) {
-                intersection++;
-                for (int i = list.size() - 1; i >= 0; i++) {
-                    list.remove(que.peek());
-                    break;
-                }
-                que.poll();
-            } else {
-                que.poll();
-                union++;
-            }
-        }
-        double jaccard = intersection / union;
-
-
-        return jaccard;
     }
 }
