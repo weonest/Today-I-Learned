6e08a8231e7be5b2a54f6ad08f257c844b6027f2
weonest <geonhee33@naver.com>
2023-04-21 20:11:05 +0900

    프로그래머스 Lv2 타겟 넘버(2분컷..?)

--- src/Solution.java
+++ src/Solution.java
@@ -1,36 +1,23 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 class Solution {
-    public int solution(String[] want, int[] number, String[] discount) {
         int answer = 0;
+    public int solution(int[] numbers, int target) {
 
-        List<String> wantList = new ArrayList<>();
+        dfs(0, target, numbers, 0);
 
-        for (int i = 0; i < want.length; i++) {
+        return answer;
+    }
 
-            for (int j = 0; j < number[i]; j++) {
-                wantList.add(want[i]);
-            }
-        }
-        // 1 2 3 4 5 6 7 8 9 10 11 12 13 14
-
-        for (int i = 0; i < discount.length - 9; i++) {
-            List<String> disList = new ArrayList<>();
-            for (int j = i; j < i + 10; j++) {
-                disList.add(discount[j]);
-            }
-
-            for (String s : wantList) {
-                disList.remove(s);
-            }
-            if (disList.isEmpty()) {
+    public void dfs(int idx, int target, int[] numbers, int tmp) {
+        if (idx == numbers.length) {
+            if (tmp == target) {
                 answer++;
             }
 
+            return;
         }
 
-        return answer;
+        dfs(idx+1, target, numbers, tmp + numbers[idx]);
+        dfs(idx+1, target, numbers, tmp - numbers[idx]);
+
     }
 }
