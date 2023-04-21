d1a168727994fd65045afe44fdc5a14f0807ad15
weonest <geonhee33@naver.com>
2023-04-21 16:21:48 +0900

    프로그래머스 Lv2 뉴스 클러스터링

--- src/Solution.java
+++ src/Solution.java
@@ -1,26 +1,80 @@
+import java.util.*;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
 class Solution {
-    public int[] solution(int[] prices) {
-        int[] answer = new int[prices.length];
+    public int solution(String str1, String str2) {
+        int answer = 0;
 
-        for (int i = 0; i < prices.length; i++) {
-            int cur = prices[i];
+        str1 = str1.toLowerCase();
+        str2 = str2.toLowerCase();
 
-            if (i == prices.length - 1) {
-                answer[i] = 0;
-                break;
+
+        List<String> list1 = new LinkedList<>();
+        List<String> list2 = new LinkedList<>();
+
+
+        String pt = "^[a-z]";
+
+        for (int i = 0; i < str1.length() - 1; i++) {
+            String cur = str1.charAt(i) + "";
+            String nex = str1.charAt(i + 1) + "";
+
+
+            if (Pattern.matches(pt, cur) && Pattern.matches(pt, nex)) {
+                list1.add(cur + nex);
             }
-            for (int j = i+1; j < prices.length; j++) {
-                int compare = prices[j];
 
-                if (cur <= compare) {
-                    answer[i]++;
-                } else {
-                    answer[i]++;
-                    break;
-                }
-            }
         }
 
+        for (int i = 0; i < str2.length() - 1; i++) {
+            String cur = str2.charAt(i) + "";
+            String nex = str2.charAt(i + 1) + "";
+
+
+            if (Pattern.matches(pt, cur) && Pattern.matches(pt, nex)) {
+                list2.add(cur + nex);
+            }
+
+        }
+
+        if (list1.size() >= list2.size()) {
+            answer = (int) Math.floor(jaccard(list1, list2) * 65536);
+        } else {
+            answer = (int) Math.floor(jaccard(list2, list1) * 65536);
+        }
+
+
         return answer;
+    }
+
+    public double jaccard(List<String> list, List<String> list2) {
+
+        Queue<String> que = new LinkedList<>(list2);
+
+        if (list.isEmpty() && list2.isEmpty()) {
+            return 1;
+        }
+
+        double intersection = 0.0;
+        double union = list.size();
+
+        while (!que.isEmpty()) {
+            if (list.contains(que.peek())) {
+                intersection++;
+                for (int i = list.size() - 1; i >= 0; i++) {
+                    list.remove(que.peek());
+                    break;
+                }
+                que.poll();
+            } else {
+                que.poll();
+                union++;
+            }
+        }
+        double jaccard = intersection / union;
+
+
+        return jaccard;
     }
 }
