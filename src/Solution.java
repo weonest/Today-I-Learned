1a9dda2c94b04df42c92c38a3bacc420282d00a7
weonest <geonhee33@naver.com>
2023-04-24 23:19:21 +0900

    프로그래머스 Lv1 키패드 누르기(카카오)

--- src/Solution.java
+++ src/Solution.java
@@ -1,37 +1,67 @@
-import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 class Solution {
-    public int solution(int n, int[] lost, int[] reserve) {
-        int answer = n - lost.length;
+    Position left;
+    Position right;
+    Position pos;
 
+    public String solution(int[] numbers, String hand) {
 
-        Arrays.sort(lost);
-        Arrays.sort(reserve);
+        StringBuilder answer = new StringBuilder();
 
-        for (int i = 0; i < lost.length; i++) {
-            for (int j = 0; j < reserve.length; j++) {
+        left = new Position(3, 0);
+        right = new Position(3, 2);
 
-                if (lost[i] == reserve[j]) {
-                    answer++;
-                    lost[i] = -1;
-                    reserve[j] = -1;
-                    break;
-                }
+        for (int i : numbers) {
+            pos = new Position((i - 1) / 3, (i - 1) % 3);
+            if (i == 0) pos = new Position(3, 1);
+
+            String finger = pos.getFinger(hand);
+
+            answer.append(finger);
+
+            if (finger.equals("L")) {
+                left = pos;
+            } else {
+                right = pos;
             }
         }
 
-        for (int i = 0; i < lost.length; i++) {
-            for (int j = 0; j < reserve.length; j++) {
-                int tmp = Math.abs(lost[i] - reserve[j]);
-                if (tmp == 1) {
-                    answer++;
-                    reserve[j] = -1;
-                    break;
-                }
-            }
-        }
-
-
-        return answer;
+        return answer.toString();
     }
+
+    class Position {
+        int row;
+        int col;
+
+        public Position(int row, int col) {
+            this.row = row;
+            this.col = col;
+        }
+
+        public String getFinger(String hand) {
+            String finger = hand.equals("right") ? "R" : "L";
+
+            if (this.col == 0) finger = "L";
+            else if (this.col == 2) finger = "R";
+            else {
+                int leftGap = left.getGap(this);
+                int rightGap = right.getGap(this);
+
+                if (leftGap < rightGap) {
+                    finger = "L";
+                } else if (leftGap > rightGap) {
+                    finger = "R";
+                }
+            }
+            return finger;
+        }
+
+        public int getGap(Position pos) {
+            return Math.abs(this.row - pos.row) + Math.abs(this.col - pos.col);
+        }
+
+    }
+
 }
