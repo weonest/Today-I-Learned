3e35c73d7747e9776285d68e7f6661878d79b06c
weonest <geonhee33@naver.com>
2023-04-25 23:01:40 +0900

    백준 1260 반복수열

--- src/Main.java
+++ src/Main.java
@@ -1,87 +1,45 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.*;
 
 public class Main {
 
-    static StringBuilder sb = new StringBuilder();
-    static boolean[] visited;
-    static int[][] arr;
-
-    static int node, line, start;
-
-    static Queue<Integer> que = new LinkedList<>();
-
-    class Node {
-        int x;
-        int y;
-
-        public Node(int x, int y) {
-            this.x = x;
-            this.y = y;
-        }
-    }
 
     public static void main(String[] args) throws IOException {
-        int sum = 0;
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        node = Integer.parseInt(st.nextToken());
-        line = Integer.parseInt(st.nextToken());
-        start = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
+        int p = Integer.parseInt(st.nextToken());
 
-        arr = new int[node + 1][node + 1];
-        visited = new boolean[node + 1];
+        List<Integer> list = new ArrayList<>();
+        list.add(N);
 
-        for (int i = 0; i < line; i++) {
-            StringTokenizer str = new StringTokenizer(br.readLine());
+        while (true) {
+            int num = 0;
+            int tmp = list.get(list.size()-1);
 
-            int a = Integer.parseInt(str.nextToken());
-            int b = Integer.parseInt(str.nextToken());
-
-            arr[a][b] = arr[b][a] = 1;
-        }
-
-        dfs(start);
-        sb.append("\n");
-
-        visited = new boolean[node + 1];
-        bfs(start);
-
-        System.out.println(sb);
-    }
-
-    public static void dfs(int start) {
-        visited[start] = true;
-        sb.append(start + " ");
-
-        for (int i = 0; i <= node; i++) {
-            if (arr[start][i] == 1 && !visited[i]) {
-                dfs(i);
+            while (tmp != 0) {
+                num += (int) Math.pow(tmp % 10, p);
+                tmp /= 10;
             }
-        }
-    }
 
-    public static void bfs(int start) {
-        que.add(start);
-        visited[start] = true;
-
-        while (!que.isEmpty()) {
-
-            start = que.poll();
-            sb.append(start + " ");
-
-            for (int i = 1; i <= node; i++) {
-                if (arr[start][i] == 1 && !visited[i]) {
-                    que.add(i);
-                    visited[i] = true;
-                }
+            if (list.contains(num)) {
+                int ans = list.indexOf(num);
+                bw.write(ans + "\n");
+                break;
             }
+
+            list.add(num);
         }
+
+        bw.flush();
+        bw.close();
+        br.close();
+
     }
+
+
 }
 
