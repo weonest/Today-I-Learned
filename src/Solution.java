6ed4c1a6684602082d6284e8c999a4c8b601222f
weonest <geonhee33@naver.com>
2023-04-28 23:33:43 +0900

    프로그래머스 Lv2 방문 길이

--- src/Solution.java
+++ src/Solution.java
@@ -1,38 +1,55 @@
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Stack;
+import java.util.*;
 
+/*
+그냥 3차원 배열 선언해서 to -> from 각각 다른 방향으로 true 표시해주기 
+*/
 
 class Solution {
+    public int solution(String dirs) {
+        int answer = 0;
+        int[] dx = {-1,0,0,1};
+        int[] dy = {0,-1,1,0};
 
-    class Number{
-        int num;
-        int idx;
+        boolean [][][] visited = new boolean[11][11][4];
 
-        public Number(int num, int idx) {
-            this.num = num;
-            this.idx = idx;
-        }
-    }
-    public int[] solution(int[] numbers) {
-        int[] answer = new int[numbers.length];
+        int x = 5;
+        int y = 5;
 
-        Arrays.fill(answer, -1);
+        for(int i = 0; i < dirs.length(); i++){
+            char in = dirs.charAt(i);
+            int dir = 0;
 
-        Stack<Number> stack = new Stack<>();
-
-        for (int i = 0; i < numbers.length; i++) {
-            if (stack.isEmpty()) {
-                stack.push(new Number(numbers[i], i));
-                continue;
+            switch(in){
+                case 'L':
+                    dir = 0;
+                    break;
+                case 'U':
+                    dir = 1;
+                    break;
+                case 'D':
+                    dir = 2;
+                    break;
+                case 'R':
+                    dir = 3;
+                    break;
+                default:
+                    break;
             }
 
-            while (!stack.empty() && numbers[i] > stack.peek().num) {
-                Number top = stack.pop();
-                answer[top.idx] = numbers[i];
-            }
-            stack.push(new Number(numbers[i], i));
+            int newX = x + dx[dir];
+            int newY = y + dy[dir];
 
+            if(newX<0||newX >=11||newY<0||newY>=11)continue;
+
+            //이 방향으로 들어온 적이 없다면 
+            if (!visited[newX][newY][3-dir]&&!visited[x][y][dir]){
+                answer++;
+                visited[newX][newY][3-dir] = true;
+                visited[x][y][dir] = true;
+            }
+
+            x = newX;
+            y = newY;
         }
 
         return answer;
