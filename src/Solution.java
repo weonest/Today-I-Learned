4e99da9ab35236c8696eabc5faa0cd4785a3c7c1
weonest <geonhee33@naver.com>
2023-04-28 22:50:05 +0900

    프로그래머스 Lv2 뒤에 있는 큰 수 찾기

--- src/Solution.java
+++ src/Solution.java
@@ -1,48 +1,40 @@
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Stack;
+
+
 class Solution {
-    int answer = 0;
 
-//    public long solution(int n) {
-//
-//
-//        dfs(n,0);
-//
-//        answer %= 1234567;
-//        return answer;
-//    }
-//
-//    public void dfs(int n, int tmp) {
-//        if (tmp >= n) {
-//            if (tmp == n) {
-//                answer++;
-//            }
-//            return;
-//        }
-//
-//        dfs(n, tmp + 1);
-//        dfs(n, tmp + 2);
-//    }
+    class Number{
+        int num;
+        int idx;
 
-    /**
-     * 위의 재귀로 푸는 식은 정답을 구할 순 있지만, 테스트 케이스에 따라 시간 초과
-     *
-     * 마지막에 1칸을 뛰어 도착하는 경우, 그 앞칸을 밟고 1칸을 뛴 경우이므로, dp[i-1] 이다.
-     *
-     * 마지막에 2칸을 뛰어 도착하는 경우, 그 앞앞칸을 밟고 2칸을 뛴 경우이므로, dp[i-2]이다.
-     *
-     * 따라서 점화식은 다음과 같다.
-     *
-     * dp[i] = dp[i-2] + dp[i-1]
-     */
-    public long solution(int n) {
-        long arr[] = new long[2001];
-        arr[1] = 1;
-        arr[2] = 2;
-
-        for (int i = 3; i < 2001; i++) {
-            arr[i] = (arr[i-1]+arr[i-2])%1234567;
+        public Number(int num, int idx) {
+            this.num = num;
+            this.idx = idx;
         }
-        return arr[n];
     }
+    public int[] solution(int[] numbers) {
+        int[] answer = new int[numbers.length];
 
+        Arrays.fill(answer, -1);
 
+        Stack<Number> stack = new Stack<>();
+
+        for (int i = 0; i < numbers.length; i++) {
+            if (stack.isEmpty()) {
+                stack.push(new Number(numbers[i], i));
+                continue;
+            }
+
+            while (!stack.empty() && numbers[i] > stack.peek().num) {
+                Number top = stack.pop();
+                answer[top.idx] = numbers[i];
+            }
+            stack.push(new Number(numbers[i], i));
+
+        }
+
+        return answer;
+    }
 }
